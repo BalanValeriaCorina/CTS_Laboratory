d9498c0e937f4bc656beb8b01f35b0ec368e0fc7
BalanValeriaCorina <corinaseliste@gmail.com>
2021-03-02 14:35:07 +0200

    Seminar 2

---
+++ Week2/src/ro/ase/csie/cts/g1094/laboratory/exeptions/IllegalTransferException.java
@@ -0,0 +1,6 @@
+package ro.ase.csie.cts.g1094.laboratory.exeptions;
+
+public class IllegalTransferException extends Exception{
+
+	
+}
---
+++ Week2/src/ro/ase/csie/cts/g1094/laboratory/exeptions/InsufficientFundsException.java
@@ -0,0 +1,16 @@
+package ro.ase.csie.cts.g1094.laboratory.exeptions;
+
+public class InsufficientFundsException extends Exception {
+
+	public InsufficientFundsException(String message)
+	{
+		super(message);
+		
+	}
+	
+	public InsufficientFundsException()
+	{
+		
+	}
+	
+}

532fa2696faaa7e7b356ceaa716df5e793d21663
BalanValeriaCorina <corinaseliste@gmail.com>
2021-03-23 14:24:15 +0200

    Week5 seminar

---
+++ Week5-Singleton/src/ro/ase/csie/cts/g1094/singleton/TestSingleton.java
@@ -0,0 +1,14 @@
+package ro.ase.csie.cts.g1094.singleton;
+
+public class TestSingleton {
+
+//	DbConnection con1=new DbConnection();
+//	
+//	DbConnection con2=new DbConnection();
+	
+	//DbConnection con1=new DbConnection("127.0.1","cts");
+	
+	//DbConnection con1=DbConnection.getConnection();
+	
+	
+}

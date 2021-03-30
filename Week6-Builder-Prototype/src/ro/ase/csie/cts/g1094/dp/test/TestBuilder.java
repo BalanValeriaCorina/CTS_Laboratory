87a8e9c7effe6f8a7ceab64b544310dfc31215fb
BalanValeriaCorina <corinaseliste@gmail.com>
2021-03-30 14:43:37 +0300

    seminar6 builder implementation

---
+++ Week6-Builder-Prototype/src/ro/ase/csie/cts/g1094/dp/test/TestBuilder.java
@@ -0,0 +1,23 @@
+package ro.ase.csie.cts.g1094.dp.test;
+
+import ro.ase.csie.cts.g1094.dp.builder.Pistol;
+import ro.ase.csie.cts.g1094.dp.builder.SuperHero;
+import ro.ase.csie.cts.g1094.dp.builder.SuperHero.SuperHeroBuilder;
+
+public class TestBuilder {
+
+	public static void main(String[] args) {
+		//SuperHero superHero=new SuperHero();
+//		superHero.name="Superman";
+//		superHero.lifePoints=100;
+//		superHero.superPower="Can fly";
+//		superHero.superPower"Laser eyes";
+		
+		//SuperHero superHero=new SuperHero("Superman",100,false,false, null, new Pistol(),"Laser eyes",null);
+		
+		SuperHero superman=new SuperHero.SuperHeroBuilder("Superman",100).addSuperPower("fly").build();
+		//superman.superPower="fly";
+		
+		SuperHero joker=new SuperHero.SuperHeroBuilder("Joke", 200).isVillain().addLeftWeapon(new Pistol()).build();
+	}
+}
